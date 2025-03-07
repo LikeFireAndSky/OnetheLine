4d4c6c98cb9c9600b1146aa884938ff68200eb38
MinSeob <icdmiracle927@gmail.com>
2025-03-07 14:53:02 +0900

    테스트 환경 구동 변경경

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,14 @@
-module.exports = {
-	presets: [
-		['@babel/preset-env', { targets: { node: 'current' } }],
-		['@babel/preset-react', { runtime: 'automatic' }], // JSX 자동 변환
-		'@babel/preset-typescript', // TypeScript 지원
-	],
-};
+// babel.config.js
+if (process.env.NODE_ENV === 'test' || process.env.JEST_WORKER_ID) {
+	// 테스트 환경에서 Babel 설정 사용
+	module.exports = {
+		presets: [
+			['@babel/preset-env', { targets: { node: 'current' } }],
+			['@babel/preset-react', { runtime: 'automatic' }],
+			'@babel/preset-typescript',
+		],
+	};
+} else {
+	// 프로덕션 빌드 시 Babel 설정 없이 SWC 사용
+	module.exports = {};
+}
